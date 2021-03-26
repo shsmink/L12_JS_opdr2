ea17e7b4ac087607b7c168d8409262135a5f38a2
shsmink <79309340+shsmink@users.noreply.github.com>
2021-03-26 17:51:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hallo Winc Academy studenten")
+
+
+/*let multiplication = 1230941 * 1823794; //dit is een grote som
+    console.log("multiplication:", multiplication);
+
+let division = 637263 / 54;
+    console.log("division:", division); */
